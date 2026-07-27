9fcb1b878e1a3e60c9fcc8010e0db386b7e7e611
Olivier Sinquin <olivier.sinquin@snowflake.com>
2026-07-27 14:40:14 -0400

    Update signup link with tracked trial URL

--- workshop_guide/app_pages/getting_started.py
+++ workshop_guide/app_pages/getting_started.py
@@ -9,7 +9,7 @@ st.markdown("#### Step 1: Sign up for a free trial")
 
 with st.container(border=True):
     st.markdown("""
-Go to **[signup.snowflake.com](https://signup.snowflake.com/?trial=...)** and fill out the registration form with your name, email, and company.
+Go to **[signup.snowflake.com](https://signup.snowflake.com/?t=cad5d6e9c8d431c2239aa81694b6b7951bff6edd23f9dc38facae3df7d11f780)** and fill out the registration form with your name, email, and company.
 
 On the **Choose your Snowflake edition** screen, select:
 
